853cef17bcfff517bdbd6deef8bb535f7fe617a2
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-07-18 17:23:50 -0400

    Refactor functions with one helper function

--- practice.js
+++ practice.js
@@ -1,44 +1,27 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  return moreOrLess(num1, num2, 1) * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return moreOrLess(num1, num2, 1) / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  return `I ate ${moreOrLess(sum1, sum2, 1)} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
+  return `I adopted a dog that weighs ${moreOrLess(weight1, weight2, 0)} pounds.`
+}
+
+
+function moreOrLess(num1, num2, size) {
+  // if size is 0, return smaller number
+  if (size === 0) {
+    return Math.min(num1, num2);
+  // else, return bigger number
   } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
+    return Math.max(num1, num2);
   }
 }
 
